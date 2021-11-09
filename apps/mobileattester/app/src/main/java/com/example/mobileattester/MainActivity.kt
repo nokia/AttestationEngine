18ad7fc07ac80e2fa0c2d4f78a98c7e54e1915c4
Cyroxin <34924561+Cyroxin@users.noreply.github.com>
2021-11-09 19:51:17 +0200

    Update kotlin version

--- apps/mobileattester/app/src/main/java/com/example/mobileattester/MainActivity.kt
+++ apps/mobileattester/app/src/main/java/com/example/mobileattester/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.mobileattester.ui.theme.MobileAttesterTheme
 import com.example.mobileattester.util.NavUtils
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalPermissionsApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
