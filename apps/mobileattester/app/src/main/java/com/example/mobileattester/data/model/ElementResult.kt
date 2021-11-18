88aede02e7206d4d0ad23ecbf5b6a2bd0612b586
Cyroxin <34924561+Cyroxin@users.noreply.github.com>
2021-11-18 15:57:02 +0200

    (mobileattester) Add element result class

---
+++ apps/mobileattester/app/src/main/java/com/example/mobileattester/data/model/ElementResult.kt
@@ -0,0 +1,34 @@
+package com.example.mobileattester.data.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Represents an Element in the application.
+ * This should contain all the data we are interested to see in the UI.
+ */
+data class ElementResult(
+    @SerializedName("_id") val resultId: String,
+    // missing "additional: []"
+    val claimID: String,
+    val elementID: String,
+    val ev: ExpectedValueElementResult,
+    val itemid: String,
+    val message: String,
+    val policyID: String,
+    val result: Int,
+    val ruleName: String,
+    val ruleParameters: HashMap<String, String>,
+    val verifiedAt: String
+)
+
+/**
+ * A10 Element impl.
+ */
+//data class ElementA10(
+//    override val itemid: String,
+//    override val name: String,
+//    override val endpoint: String,
+//    override val description: String?,
+//    override val protocol: String,
+//    @SerializedName("type") override val types: List<String>,
+//) : Element
