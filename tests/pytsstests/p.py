e9de4384e7dbde57912c7ff30bdc86c2325570b0
Ian <ian.justin.oliver@gmail.com>
2022-01-18 22:52:00 +0200

    minor updates

---
+++ tests/pytsstests/p.py
@@ -0,0 +1,11 @@
+import tpm2_pytss
+import inspect
+
+with tpm2_pytss.ESAPI("mssim:host=localhost,port=2321") as t:
+	print("T is ",t)
+
+	pcrs = tpm2_pytss.TPML_PCR_SELECTION.parse("sha1:0,1,2")
+	s = t.pcr_read(pcrs)
+
+	print("Returned PCR structure is ",str(s[1]))
+	print(inspect.getmembers(s[1]))
