7757204eb306f581d0773da4ad04f2b38df87086
ioliver <ian.oliver@nokia-bell-labs.com>
2022-03-02 21:19:35 +0200

    dockerfile local updates

--- t10/nut10/run_t10_as_podman_container.sh
+++ t10/nut10/run_t10_as_podman_container.sh
@@ -3,4 +3,4 @@
 #Licensed under the BSD 3-Clause Clear License.
 #SPDX-License-Identifier: BSD-3-Clear
 
-podman run -p 8530:8530 --device=/dev/tpm0 nut10
+podman run -p 8530:8530 --device=/dev/tpm0  --group-add keep-groups nut10
