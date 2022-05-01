f5fb117e85fa64a306e4780250d7cfdd231c9c5b
Ian <ian.justin.oliver@gmail.com>
2022-05-01 23:19:27 +0300

    fixed a few minor typos

--- a10server/a10/asvr/types.py
+++ a10server/a10/asvr/types.py
@@ -2,12 +2,6 @@
 # Licensed under the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import a10.structures.constants
-import a10.structures.identity
-import a10.structures.returncode
-import a10.asvr.db.core
-import a10.asvr.db.announce
-
 import a10.asvr.elements
 
 
@@ -19,16 +13,11 @@ def getTypes():
 
     """
 
-    ts = []
+    ts = set()
     es = a10.asvr.elements.getElementsFull()
 
     for e in es:
-        print("type=", e["type"],type(e["type"]))
-        # ugly - we keep everything as a flat list
         for t in e["type"]:
-            ts.append(t)
-        print("ts  =", ts)
-        # otherwise the set(List) breaks
-        # warned you!
+            ts.add(t)
 
-    return set(ts)
+    return ts
