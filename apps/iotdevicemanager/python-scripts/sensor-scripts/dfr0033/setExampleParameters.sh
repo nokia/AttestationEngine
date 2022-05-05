7a4aa2eb3f8d64272bb296e5ac43746884db73d9
aleksikosonen <kososenaleksi@gmail.com>
2022-05-05 20:03:13 +0300

    Delete unused Python scripts

--- apps/iotdevicemanager/python-scripts/sensor-scripts/dfr0033/setExampleParameters.sh
+++
@@ -1,3 +0,0 @@
-#/!/bin/sh
-
-export DFR0033_PARAMS="{ 'rate':10, 'mqttbrokerip':'', 'mqttbrokerport':'', 'mqttchannel':'magnet' }"
--- apps/iotdevicemanager/python-scripts/sensor-scripts/ds18b20/setExampleParameters.sh
+++
@@ -1,3 +0,0 @@
-#/!/bin/sh
-
-export DS18B20_PARAMS="{ 'rate':10, 'mqttbrokerip':'192.168.86.25', 'mqttbrokerport':'1883', 'mqttchannel':'temperaturehumidity' }"
