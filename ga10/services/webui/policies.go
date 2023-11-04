6625304ba74229f988b4f222c42698f7f3711403
Thore Sommer <mail@thson.de>
2023-11-04 09:16:11 +0200

    webui: enable submission of policies

--- ga10/services/webui/policies.go
+++ ga10/services/webui/policies.go
@@ -1,25 +1,45 @@
 package webui
 
-import(
+import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 
-    "a10/operations"
+	"a10/operations"
+	"a10/structures"
 )
 
-
 func showPolicies(c echo.Context) error {
-	es,_ := operations.GetPoliciesAll()
-	return c.Render(http.StatusOK, "policies.html",es)
+	es, _ := operations.GetPoliciesAll()
+	return c.Render(http.StatusOK, "policies.html", es)
 }
 
 func showPolicy(c echo.Context) error {
-	e,_ := operations.GetPolicyByItemID(c.Param("itemid"))
-	return c.Render(http.StatusOK, "policy.html",e)
+	e, _ := operations.GetPolicyByItemID(c.Param("itemid"))
+	return c.Render(http.StatusOK, "policy.html", e)
 }
 
-
 func newPolicy(c echo.Context) error {
-	return c.Render(http.StatusOK, "editpolicy.html",nil)
+	return c.Render(http.StatusOK, "editpolicy.html", nil)
+}
+
+func processNewPolicy(c echo.Context) error {
+	elemdata := c.FormValue("policydata")
+
+	var newPolicy structures.Policy
+
+	err := json.Unmarshal([]byte(elemdata), &newPolicy)
+
+	if err != nil {
+		fmt.Printf("error is %v\n", err.Error())
+		return c.Redirect(http.StatusSeeOther, "/new/policy")
+	}
+
+	fmt.Printf("  fv%v\n", newPolicy)
+	eid, err := operations.AddPolicy(newPolicy)
+	fmt.Printf("  eid=%v,err=%v\n", eid, err)
+
+	return c.Redirect(http.StatusSeeOther, "/policies")
 }
