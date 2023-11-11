bceccf7a550959c7e1b3246ef7cf10b513ea7f81
Thore Sommer <mail@thson.de>
2023-11-11 16:54:04 +0200

    ga10: remove name and use marshaled TPM structure for Public instead

--- ga10/structures/elements.go
+++ ga10/structures/elements.go
@@ -1,47 +1,47 @@
 package structures
 
 type Element struct {
-        ItemID string           `json:"itemid,omitempty",bson:"itemid,omitempty"`
-        Name string             `json:"name",bson:"name"`
-        Description string      `json:"description",bson:"description"`
-        Endpoint string         `json:"endpoint",bson:"endpoint"`
-        Protocol string         `json:"protocol",bson:"protocol"`
-        Tags []string           `json:"tags",bson:"tags"`
+	ItemID      string   `json:"itemid,omitempty",bson:"itemid,omitempty"`
+	Name        string   `json:"name",bson:"name"`
+	Description string   `json:"description",bson:"description"`
+	Endpoint    string   `json:"endpoint",bson:"endpoint"`
+	Protocol    string   `json:"protocol",bson:"protocol"`
+	Tags        []string `json:"tags",bson:"tags"`
 
-        Sshkey SSHKEY          `json:"sshkey,omitempty",bson:"sshkey,omitempty"`
-        TPM2 TPM2              `json:"tpm2,omitempty",bson:"tpm2,omitempty"`
-        UEFI UEFI              `json:"uefi,omitempty",bson:"uefi,omitempty"`
-        IMA IMA                `json:"ima,omitempty",bson:"ima,omitempty"`
-        TXT TXT                `json:"txt,omitempty",bson:"txt,omitempty"`
+	Sshkey SSHKEY `json:"sshkey,omitempty",bson:"sshkey,omitempty"`
+	TPM2   TPM2   `json:"tpm2,omitempty",bson:"tpm2,omitempty"`
+	UEFI   UEFI   `json:"uefi,omitempty",bson:"uefi,omitempty"`
+	IMA    IMA    `json:"ima,omitempty",bson:"ima,omitempty"`
+	TXT    TXT    `json:"txt,omitempty",bson:"txt,omitempty"`
 }
 
 type SSHKEY struct {
-        Key string              `json:"key",bson:"key"`
-        Timeout int16           `json:"timeout",bson:"timeout"`
-        Username string         `json:"username",bson:"username"`        
+	Key      string `json:"key",bson:"key"`
+	Timeout  int16  `json:"timeout",bson:"timeout"`
+	Username string `json:"username",bson:"username"`
 }
 
 type UEFI struct {
-        Eventlog string          `json:"eventlog",bson:"eventlog"`       
+	Eventlog string `json:"eventlog",bson:"eventlog"`
 }
 
 type IMA struct {
-        ASCIILog string          `json:"asciilog",bson:"asciilog"`       
+	ASCIILog string `json:"asciilog",bson:"asciilog"`
 }
 
 type TXT struct {
-        Log string               `json:"log",bson:"log"`       
+	Log string `json:"log",bson:"log"`
 }
 
 type TPM2 struct {
-        Device string            `json:"device",bson:"device"`    
-        EKCertHandle string      `json:"ekcerthandle",bson:"ekcerthandle"`
-        EK TPMKey                `json:"ek",bson:"ek"`    
-        AK TPMKey                `json:"ak",bson:"ak"`    
+	Device       string `json:"device",bson:"device"`
+	EKCertHandle string `json:"ekcerthandle",bson:"ekcerthandle"`
+	EK           TPMKey `json:"ek",bson:"ek"`
+	AK           TPMKey `json:"ak",bson:"ak"`
 }
 
 type TPMKey struct {
-        Public string             `json:"public",bson:"public"`    
-        Handle string             `json:"handle",bson:"handle"`    
-        Name string               `json:"name",bson:"name"`    
+	Handle string `json:"handle",bson:"handle"`
+	// Public portion of the key marshalled as TPM2BPublic
+	Public string `json:"public",bson:"public"`
 }
