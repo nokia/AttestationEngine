820755e5da59c55e9569105d9e7fc7efaec85576
Thore Sommer <mail@thson.de>
2024-01-22 17:43:24 +0100

    ta10: fix location for MB log

--- ta10/uefi/endpoints.go
+++ ta10/uefi/endpoints.go
@@ -1,35 +1,33 @@
 package uefi
 
-import(
-	"net/http"
+import (
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
-	"encoding/base64"
+	"net/http"
 
-	"ta10/common"
+	utilities "ta10/common"
 
 	"github.com/labstack/echo/v4"
 )
 
-
-const UEFIEVENTLOGLOCATION string = "/sys/kernel/ima/ascii_runtime_measurements"
-
+const UEFIEVENTLOGLOCATION string = "/sys/kernel/security/tpm0/binary_bios_measurements"
 
 type returnEventLog struct {
-	EventLog    string       `json:"eventlog"`
-	Encoding   string        `json:"encoded"`
+	EventLog        string `json:"eventlog"`
+	Encoding        string `json:"encoded"`
 	UnEncodedLength int    `json:"unencodedlength"`
-	EncodedLength int      `json:"encodedlength"`
+	EncodedLength   int    `json:"encodedlength"`
 }
 
 func GetEventLogLocation(loc string) string {
-	fmt.Printf("UEFI Log requested from %v, unsafe mode is %v, giving: ",loc,utilities.IsUnsafe())
+	fmt.Printf("UEFI Log requested from %v, unsafe mode is %v, giving: ", loc, utilities.IsUnsafe())
 
-	if utilities.IsUnsafe()==true {
-		fmt.Printf("%v\n",loc)
+	if utilities.IsUnsafe() == true {
+		fmt.Printf("%v\n", loc)
 		return loc
 	} else {
-		fmt.Printf("%v\n",UEFIEVENTLOGLOCATION)
+		fmt.Printf("%v\n", UEFIEVENTLOGLOCATION)
 		return UEFIEVENTLOGLOCATION
 	}
 }
@@ -37,23 +35,23 @@ func GetEventLogLocation(loc string) string {
 func Eventlog(c echo.Context) error {
 	fmt.Println("eventlog called")
 
-	var postbody  map[string]interface{}
-	var rtnbody = make( map[string]interface{} )
+	var postbody map[string]interface{}
+	var rtnbody = make(map[string]interface{})
 
-	if err := c.Bind(&postbody); err != nil {	
-		rtnbody["postbody"] = err.Error() 
+	if err := c.Bind(&postbody); err != nil {
+		rtnbody["postbody"] = err.Error()
 		return c.JSON(http.StatusUnprocessableEntity, rtnbody)
 	}
 
-	u := GetEventLogLocation(fmt.Sprintf("%v",postbody["uefi/eventlog"]))
-	
-	fcontent,err := ioutil.ReadFile(u)
+	u := GetEventLogLocation(fmt.Sprintf("%v", postbody["uefi/eventlog"]))
+
+	fcontent, err := ioutil.ReadFile(u)
 	if err != nil {
-		rtnbody["file err"]=err.Error() 
+		rtnbody["file err"] = err.Error()
 		return c.JSON(http.StatusInternalServerError, rtnbody)
 	}
 	scontent := base64.StdEncoding.EncodeToString(fcontent)
 
-	rtn := returnEventLog{ scontent, "base64", len(fcontent), len(scontent) }
+	rtn := returnEventLog{scontent, "base64", len(fcontent), len(scontent)}
 	return c.JSON(http.StatusOK, rtn)
 }
